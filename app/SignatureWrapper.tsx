aaf72fabd2ee3389c2f34768dc6ea073e5f65472
dk1a <dk1a@protonmail.com>
2025-12-10 14:58:28 +0300

    simple sign

--- app/SignatureWrapper.tsx
+++ app/SignatureWrapper.tsx
@@ -1,12 +1,13 @@
 import { Signature } from '@coinbase/onchainkit/signature';
 import { useState } from 'react';
-import { toHex } from 'viem';
+import { signTypedData } from 'viem/actions';
 import { base } from 'viem/chains';
-import { useAccount } from 'wagmi';
+import { getAction, toHex } from 'viem/utils';
+import { useConnectorClient } from 'wagmi';
 
 export default function SignatureWrapper() {
   const [signature, setSignature] = useState<string | null>(null);
-  const { address } = useAccount()
+  const { data: connectorClient } = useConnectorClient()
 
   const domain = {
     verifyingContract: "0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a",
@@ -23,17 +24,40 @@ export default function SignatureWrapper() {
     ],
   } as const;
 
-  const message = {
-    signer: address,
-    systemNamespace: "world",
-    systemName: "RegistrationSystem",
-    callData: "0x01020304050607080910",
-    nonce: 2n
-  } as const;
+  async function onClick() {
+    if (!connectorClient) {
+      setSignature("error: no client");
+      return;
+    }
+
+    const message = {
+      signer: connectorClient.account.address,
+      systemNamespace: "world",
+      systemName: "RegistrationSystem",
+      callData: "0x01020304050607080910",
+      nonce: 2n
+    } as const;
+
+    const result = await getAction(
+      connectorClient,
+      signTypedData,
+      "signTypedData"
+    )({
+      account: connectorClient.account,
+      // EIP-712 domain bound to World contract and chain
+      domain,
+      // MUD's CallWithSignature type definitions
+      types,
+      primaryType: "Call",
+      // Message contains all call details + nonce
+      message
+    })
+    setSignature(result);
+  }
 
   return (
     <div>
-      <Signature
+      {/*<Signature
         domain={domain}
         types={types}
         primaryType="Call"
@@ -41,7 +65,8 @@ export default function SignatureWrapper() {
         label="Sign EIP712"
         onSuccess={(signature: string) => setSignature(signature)}
         onError={(e) => console.error(e)}
-      />
+      />*/}
+      <button onClick={onClick}>Sign EIP-712</button>
       <p style={{ overflowWrap: "anywhere" }}>Signature: {signature}</p>
     </div>
   );
