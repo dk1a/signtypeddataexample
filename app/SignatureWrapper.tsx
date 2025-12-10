29c8f0d1c17053ab2418d9680f0c1501822807e9
dk1a <dk1a@protonmail.com>
2025-12-10 16:47:18 +0300

    more buttons

--- app/SignatureWrapper.tsx
+++ app/SignatureWrapper.tsx
@@ -26,19 +26,33 @@ export default function SignatureWrapper() {
     ],
   } as const;
 
-  async function onClick() {
-    if (!connectorClient) {
-      setSignature("error: no client");
-      return;
-    }
-
-    const message = {
-      signer: connectorClient.account.address,
+  const message = {
+      signer: connectorClient?.account.address,
       systemNamespace: "world",
       systemName: "RegistrationSystem",
       callData: "0x01020304050607080910",
       nonce: 2n
     } as const;
+
+  async function onClick1() {
+    const result = await signTypedDataAsync({
+      account: connectorClient?.account,
+      // EIP-712 domain bound to World contract and chain
+      domain,
+      // MUD's CallWithSignature type definitions
+      types,
+      primaryType: "Call",
+      // Message contains all call details + nonce
+      message
+    })
+    setSignature(result);
+  }
+
+  async function onClick2() {
+    if (!connectorClient) {
+      setSignature("error: no client");
+      return;
+    }
 
     const action = getAction(
       connectorClient,
@@ -46,8 +60,8 @@ export default function SignatureWrapper() {
       "signTypedData"
     )
 
-    const result = await signTypedDataAsync({
-      account: connectorClient.account,
+    const result = await action({
+      account: connectorClient?.account,
       // EIP-712 domain bound to World contract and chain
       domain,
       // MUD's CallWithSignature type definitions
@@ -60,17 +74,27 @@ export default function SignatureWrapper() {
   }
 
   return (
-    <div>
-      {/*<Signature
+    <div style={{ color: "white" }}>
+      <div>
+        signer address: {connectorClient?.account.address}
+      </div>
+      <Signature
         domain={domain}
         types={types}
         primaryType="Call"
-        message={message}
+        message={{
+          signer: connectorClient?.account.address,
+          systemNamespace: "world",
+          systemName: "RegistrationSystem",
+          callData: "0x01020304050607080910",
+          nonce: 2n
+        }}
         label="Sign EIP712"
         onSuccess={(signature: string) => setSignature(signature)}
         onError={(e) => console.error(e)}
-      />*/}
-      <button onClick={onClick}>Sign EIP-712</button>
+      />
+      <button onClick={onClick1}>Sign 1</button>
+      <button onClick={onClick2}>Sign 2</button>
       <p style={{ overflowWrap: "anywhere" }}>Signature: {signature}</p>
     </div>
   );
