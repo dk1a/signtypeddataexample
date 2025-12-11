45be2e475117ff5cb699bbd5a8addc406fe201e2
dk1a <dk1a@protonmail.com>
2025-12-11 15:45:27 +0300

    no version

--- app/SignatureWrapper.tsx
+++ app/SignatureWrapper.tsx
@@ -11,7 +11,7 @@ export default function SignatureWrapper() {
   const domain = {
     name: 'test123',
     //version: '1.0.0',
-    //chainId: base.id,
+    chainId: base.id,
     verifyingContract: '0x28d10E6aAb1a749Be792b4D8aa0519c70E83386a',
     salt: toHex(base.id, { size: 32 })
   } as const;
